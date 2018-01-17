f4990c8f70a175bc80c9292ea798e31f5998f3f1
Andre Saiko <Maplpapl@users.noreply.github.com>
2018-01-17 21:44:10 +0100

    test

---
+++ js/javascript.js
@@ -0,0 +1,11 @@
+
+function begruessung() {
+    alert("Willkommen bei JavaScript");
+}
+
+function potenz() {
+  document.write("<h1>Aufgabe</h1>");
+  for ( i=1; i<25; i++) {
+    document.write("Quadrat von " + i + " ist: " + Math.pow(i,2) + "<br />");
+  }
+}
